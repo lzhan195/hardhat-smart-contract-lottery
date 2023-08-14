38497f2ab3c962c89688bd74374cac1574333eb8
lzhan195 <longtengzhang77@gmail.com>
2023-08-14 19:48:40 -0400

    add checkUpKeep from Chainlink Keepers

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -4,11 +4,19 @@ pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatiableInterface.sol";
 
 error Lottery_NotEnoughETHEntered();
 error Lottery_TransferFailed();
+error Lottery_NotOpen();
 
-contract Lottery is VRFConsumerBaseV2 {
+contract Lottery is VRFConsumerBaseV2, KeeperCompatiableInterface {
+    /* Type Declarations */
+    enum LotteryState {
+        OPEN,
+        CALCULATING
+    }
+
     /* State Variables*/
     uint256 private immutable i_enteranceFee;
     address payable[] private s_players;
@@ -21,6 +29,9 @@ contract Lottery is VRFConsumerBaseV2 {
 
     /* Lottery Variables */
     address private s_recentWinner;
+    LotteryState private s_lotteryState;
+    uint256 private s_lastTimeStamp;
+    uint256 private immutable i_interval;
 
     /* Events */
     event LotteryEnter(address indexed player);
@@ -32,13 +43,17 @@ contract Lottery is VRFConsumerBaseV2 {
         uint256 enteranceFee,
         bytes32 gasLane,
         uint64 subscriptionId,
-        uint32 callbackGasLimit
+        uint32 callbackGasLimit,
+        uint256 interval
     ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_enteranceFee = enteranceFee;
         i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
         i_gasLane = gasLane;
         i_subscriptionId = subscriptionId;
         i_CallbackGasLimit = callbackGasLimit;
+        s_lotteryState = LotteryState.OPEN;
+        s_lastTimeStamp = block.timestamp;
+        i_interval = interval;
     }
 
     function enterLottery() public payable {
@@ -46,15 +61,32 @@ contract Lottery is VRFConsumerBaseV2 {
         if (msg.value < i_enteranceFee) {
             revert Lottery_NotEnoughETHEntered();
         }
+
+        if (s_lotteryState != LotteryState.OPEN) {
+            revert Lottery_NotOpen();
+        }
         s_players.push(payable(msg.sender));
         //Emit an event when we update a dynamic array or mapping
         //Named events with the function name reversed
         emit LotteryEnter(msg.sender);
     }
 
+    /* @dev This is the function thta the Chainlink Keeper nodes call 
+    they look for the `upkeepNeeded` to return true */
+    function checkUpKeep(
+        bytes calldata /*checkData*/
+    ) external override returns (bool upkeepNeeded, bytes memory /*performData*/) {
+        bool isOpen = (LotteryState.OPEN == s_lotteryState);
+        bool timePassed = ((block.timestamp - s_lastTimeStamp) > i_interval);
+        bool hasPlayers = (s_players.length > 0);
+        bool hasBalance = address(this).balance > 0;
+        upkeepNeeded = (isOpen && timePassed && hasPlayers && hasBalance);
+    }
+
     function requestRandomWinner() external {
         //Resquest the random number
         //Once get it, do something
+        s_lotteryState = LotteryState.CALCULATING;
         uint256 requestId = i_vrfCoordinator.requestRandomWords(
             i_gasLane,
             i_subscriptionId,
@@ -72,6 +104,8 @@ contract Lottery is VRFConsumerBaseV2 {
         uint256 indexOfWinner = randomWords[0] % s_players.length;
         address payable recentWinner = s_players[indexOfWinner];
         s_recentWinner = recentWinner;
+        s_lotteryState = LotteryState.OPEN;
+        s_players = new address payable[](0);
         (bool success, ) = recentWinner.call{value: address(this).balance}("");
         // require(success)
         if (!success) {
